32598667c7b37a76d698c8612469920186b445b2
Endy4elect <endy4elect@yahoo.com>
2022-12-15 15:32:55 -0800

    push.c

--- push.c
+++ push.c
@@ -18,13 +18,16 @@ void f_push(stack_t **head, unsigned int counter)
 		for (; bus.arg[m] != '\0'; m++)
 		{
 			if (bus.arg[m] > 57 || bus.arg[m] < 48)
-				flag = 1; }
+				flag = 1; 
+		}
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE); }}
+			exit(EXIT_FAILURE); 
+		}
+	}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
